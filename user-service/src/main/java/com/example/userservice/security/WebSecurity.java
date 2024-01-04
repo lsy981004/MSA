157046324691ec9343ef86acba632e7c6a9c6c5c
lsy981004 <lsy981004@gmail.com>
2024-01-04 17:47:12 +0900

    240104

--- user-service/src/main/java/com/example/userservice/security/WebSecurity.java
+++ user-service/src/main/java/com/example/userservice/security/WebSecurity.java
@@ -1,32 +1,66 @@
 package com.example.userservice.security;
 
 
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import com.example.userservice.service.UserService;
+
+import lombok.RequiredArgsConstructor;
+
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.AuthenticationManager;
+
+import org.springframework.security.authorization.AuthorizationDecision;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.security.web.util.matcher.IpAddressMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
+
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurity {
-    private static final String[] WHITE_LIST = {
-            "/users/**",
-            "/**"
-    };
+@RequiredArgsConstructor
+public class WebSecurity{
+    private final AuthenticationConfiguration authConfig;
+    private final UserService userService;
+    private final Environment environment;
 
     @Bean
-    protected SecurityFilterChain config(HttpSecurity http) throws Exception {
-        return http.csrf(CsrfConfigurer::disable)
-                .headers(authorize -> authorize
-                        .frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(WHITE_LIST).permitAll()
-                        .requestMatchers(PathRequest.toH2Console()).permitAll())
-                .getOrBuild();
+    public MvcRequestMatcher.Builder mvc(HandlerMappingIntrospector introspector) {
+        return new MvcRequestMatcher.Builder(introspector);
     }
 
+    @Bean
+    protected SecurityFilterChain configure(HttpSecurity http, MvcRequestMatcher.Builder mvc) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable);
+//        http.authorizeHttpRequests(authorize -> authorize.requestMatchers(AntPathRequestMatcher.antMatcher("/**")).permitAll());
+        IpAddressMatcher hasIpv4 = new IpAddressMatcher("127.0.0.1");
+        IpAddressMatcher hasIpv6 = new IpAddressMatcher("0:0:0:0:0:0:0:1");
+        http.authorizeHttpRequests(authorize -> authorize.requestMatchers(mvc.pattern("/actuator/**")).permitAll());
+        http.authorizeHttpRequests(authorize ->
+                authorize.requestMatchers(mvc.pattern("/**"))
+                        .access((authentication, object) ->
+                                new AuthorizationDecision(hasIpv4.matches(object.getRequest()) || hasIpv6.matches(object.getRequest()))
+                        )
+        ).addFilter(getAuthenticationFilter());
+
+        http.headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
+
+        return http.build();
+    }
+
+    private AuthenticationFilter getAuthenticationFilter() throws Exception {
+        AuthenticationManager authenticationManager = authConfig.getAuthenticationManager();
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter(authenticationManager,
+                userService, environment);
+        return authenticationFilter;
+    }
 }
